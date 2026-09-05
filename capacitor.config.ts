d05cbdcd6b604fed01141e600bd256da52e5e349
Meochrist <148457870+Meochrist@users.noreply.github.com>
2026-09-05 22:46:46 +0100

    android: build APK final installé sur device

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.mannadaily.app',
   appName: 'MannaDaily',
   webDir: 'out',
+  server: {
+    url: 'https://mannadaily.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
